50fef18cb40aab1b3462d9d4187d793ed7db9f95
zafle <fleutotanne@free.fr>
2024-09-11 10:58:39 +0200

    vertical layout test : add WHEN

--- src/__tests__/VerticalLayout.js
+++ src/__tests__/VerticalLayout.js
@@ -8,16 +8,17 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 
 
 describe('Given I am connected as Employee', () => {
-  test("Then Icons should be rendered", () => {
-    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-    const user = JSON.stringify({
-      type: 'Employee'
+  describe('When I am on a page showing vertical layout', () => {
+    test("Then Icons should be rendered", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      const user = JSON.stringify({
+        type: 'Employee'
+      })
+      window.localStorage.setItem('user', user)
+      const html = VerticalLayout(120)
+      document.body.innerHTML = html
+      expect(screen.getByTestId('icon-window')).toBeTruthy()
+      expect(screen.getByTestId('icon-mail')).toBeTruthy()
     })
-    window.localStorage.setItem('user', user)
-    const html = VerticalLayout(120)
-    document.body.innerHTML = html
-    expect(screen.getByTestId('icon-window')).toBeTruthy()
-    expect(screen.getByTestId('icon-mail')).toBeTruthy()
   })
-
 })
